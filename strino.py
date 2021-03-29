4f0b69fa4fe4212b2e3285f1cdabff32ff44d8df
jeffersson <ctw6av@gmail.com>
2021-03-28 22:12:38 -0400

    Added check for root privileges.

--- strino.py
+++ strino.py
@@ -26,6 +26,12 @@ import os
 
 if __name__ == '__main__':
 
+    # Check if user has root access.
+    # All operations need root
+    if os.geteuid():
+        print('We need root access!')
+        sys.exit(1)
+
     # Get the default server from settings
     config = configparser.ConfigParser()
     config.read('settings.ini')
